9ce3a179be9b435a85f46134da2e825e83ec168d
KonH <konh@yandex.ru>
2016-11-09 14:29:41 +0600

    Modified strategy;

--- MyStrategy.cs
+++ MyStrategy.cs
@@ -29,8 +29,13 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk {
 
 	public sealed class MyStrategy : IStrategy {
 
-		const double LowHPFactor = 0.25D;
+		const double SpawnDistance = 1000D;
+		const double MinMoveDistance = 2.25D;
+		const double LowHPFactor = 0.50D;
+		const double SafeHPFactor = 0.85D;
+		const double MeleeDistance = 175D;
 		const double WaypointRadius = 100D;
+		const double StrafeCoeff = 4D;
 
 		Wizard self;
 		World world;
@@ -41,18 +46,33 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk {
 		Dictionary<LaneType, Vector2[]> waypointsByLane;
 		LaneType curLane;
 		Vector2[] curWaypoints;
+		bool strafeDir = false;
+		double strafeValue = 0;
+
+		double detectDistance;
+		Vector2 prevPos = new Vector2(0, 0);
+		double lastDistance = 0D;
+		bool spawned = true;
+		bool lowHp = false;
+		bool safeHp = false;
+		bool meleeDanger = false;
+		bool enemyOverwaight = false;
+		List<Minion> nearMinions = new List<Minion>();
+		List<Wizard> nearWizards = new List<Wizard>();
+		List<Building> nearBuildings = new List<Building>();
+		List<Wizard> nearFriends = new List<Wizard>();
+		List<Minion> nearNeutrals = new List<Minion>();
+		List<Tree> nearTrees = new List<Tree>();
+		string prevAction = "";
+		string curAction = "";
 
 		public void Move(Wizard self, World world, Game game, Move move) {
 			SetupTick(self, world, game, move);
 			TryInit(self, game);
-			SetStrafe();
-			if ( IsLowHP() ) {
-				MoveTo(GetPreviousWaypoint());
-			} else {
-				if ( !TryAttack() ) {
-					MoveTo(GetNextWaypoint());
-				}
-			}
+			CheckSpawn();
+			UpdateUnits();
+			UpdateConditions();
+			MakeAction();
 		}
 
 		void SetupTick(Wizard self, World world, Game game, Move move) {
@@ -62,61 +82,46 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk {
 			this.move = move;
 		}
 
-		bool IsLowHP() {
-			return self.Life < self.MaxLife * LowHPFactor;
-		}
-
-		// Attack
-		LivingUnit GetNearestTarget() {
-			var targets = new List<LivingUnit>();
-			targets.AddRange(world.Buildings);
-			targets.AddRange(world.Wizards);
-			targets.AddRange(world.Minions);
-
-			LivingUnit nearestTarget = null;
-			double nearestTargetDistance = double.MaxValue;
-
-			foreach ( var target in targets ) {
-				if ( target.Faction == Faction.Neutral || target.Faction == self.Faction ) {
-					continue;
-				}
-
-				double distance = self.GetDistanceTo(target);
-
-				if ( distance < nearestTargetDistance ) {
-					nearestTarget = target;
-					nearestTargetDistance = distance;
-				}
-			}
-
-			return nearestTarget;
-		}
-
-		bool TryAttack() {
-			LivingUnit nearestTarget = GetNearestTarget();
-
-			if ( nearestTarget != null ) {
-				double distance = self.GetDistanceTo(nearestTarget);
-
-				if ( distance <= self.CastRange ) {
-					double angle = self.GetAngleTo(nearestTarget);
-					move.Turn = angle;
-					if ( Math.Abs(angle) < game.StaffSector / 2.0D ) {
-						move.Action = ActionType.MagicMissile;
-						move.CastAngle = angle;
-						move.MinCastDistance = distance - nearestTarget.Radius + game.MagicMissileRadius;
-					}
-
-					return true;
-				}
-			}
-			return false;
-		}
-
 		// Move
+		void CheckSpawn() {
+			lastDistance = self.GetDistanceTo(prevPos.X, prevPos.Y);
+			spawned = lastDistance > SpawnDistance;
+			prevPos = new Vector2(self.X, self.Y);
+			if ( spawned ) {
+				SelectLane();
+			}
+		}
+
+		void SelectLane() {
+			switch ( random.Next(4) ) {
+				case 0:
+					curLane = LaneType.Top;
+					break;
+				case 1:
+					curLane = LaneType.Bottom;
+					break;
+				case 2:
+				case 3:
+					curLane = LaneType.Middle;
+					break;
+			}
+
+			curWaypoints = waypointsByLane[curLane];
+			Console.WriteLine("Select lane: " + curLane);
+		}
+
+		bool NeedChangeStrafeDir() {
+			return (strafeValue > self.Radius * StrafeCoeff);
+		}
+
 		void SetStrafe() {
 			var strafe = game.WizardStrafeSpeed;
-			move.StrafeSpeed = GetRandomBool() ? strafe : -strafe;
+			strafeValue += strafe;
+			if( NeedChangeStrafeDir() ) {
+				strafeDir = !strafeDir;
+				strafeValue = 0;
+			}
+			move.StrafeSpeed = strafeDir ? strafe : -strafe;
 		}
 
 		void MoveTo(Vector2 point) {
@@ -126,6 +131,8 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk {
 
 			if ( Math.Abs(angle) < game.StaffSector / 4.0D ) {
 				move.Speed = game.WizardForwardSpeed;
+			} else {
+				move.Speed = game.WizardBackwardSpeed;
 			}
 		}
 
@@ -166,6 +173,268 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk {
 			return firstWaypoint;
 		}
 
+		// Conditions
+		void UpdateUnits() {
+			detectDistance = self.VisionRange;
+			SelectNearUnits(world.Buildings, EnemyFaction, nearBuildings);
+			SelectNearUnits(world.Minions, EnemyFaction, nearMinions);
+			SelectNearUnits(world.Wizards, EnemyFaction, nearWizards);
+			SelectNearUnits(world.Wizards, FriendFaction, nearFriends);
+			SelectNearUnits(world.Minions, NeutralFaction, nearNeutrals);
+			SelectNearUnits(world.Trees, OtherFaction, nearTrees);
+		}
+
+		void UpdateConditions() {
+			lowHp = self.Life < self.MaxLife * LowHPFactor;
+			safeHp = self.Life > self.MaxLife * SafeHPFactor;
+			meleeDanger = IsMeleeDanger();
+			enemyOverwaight = nearWizards.Count > nearFriends.Count;
+		}
+
+		bool IsMeleeDanger() {			
+			return HasMeleeDanger(nearMinions) || HasMeleeDanger(nearWizards) || HasMeleeDanger(nearNeutrals);
+		}
+
+		bool HasMeleeDanger<T>(List<T> units) where T:LivingUnit {
+			var nearestUnit = GetNearestUnit(units);
+			if( nearestUnit != null ) {
+				return self.GetDistanceTo(nearestUnit) <= MeleeDistance + self.Radius;
+			}
+			return false;
+		}
+
+		// Actions
+		void SetAction(string action) {
+			curAction = action;
+			if( action != prevAction ) {
+				Console.WriteLine(string.Format("Action: {0}", action));
+			}
+		}
+
+		void MakeAction() {
+			prevAction = curAction;
+			curAction = "";
+			if( lowHp ) {
+				Retreat("low HP");
+			} else {
+				if( meleeDanger ) {
+					if ( nearWizards.Count > 0 ) {
+						Retreat("melee danger");
+					} else if ( safeHp ) {
+						if( nearMinions.Count > 0 ) {
+							AttackMinion(true);
+						} else if ( nearNeutrals.Count > 0 ) {
+							AttackNeutral();
+						}
+					} else {
+						Retreat("melee danger");
+					}
+				}
+				if ( curAction == "" ) {
+					if ( nearWizards.Count > 0 ) {
+						if ( enemyOverwaight ) {
+							if ( safeHp ) {
+								AttackWizard();
+							} else {
+								Retreat("enemy overwaight");
+							}
+						} else {
+							AttackWizard();
+						}
+					} else {
+						if ( nearBuildings.Count > 0 ) {
+							AttackBuilding();
+						} else if ( nearMinions.Count > 0 ) {
+							AttackMinion(false);
+						} else if ( nearNeutrals.Count > 0 ) {
+							AttackNeutral();
+						} else {
+							Move();
+						}
+					}
+				}
+			}
+			if( curAction == "" ) {
+				SetAction("None");
+			}
+		}
+
+		void Move() {
+			MoveTo(GetNextWaypoint());
+			if( lastDistance < MinMoveDistance ) {
+				SetStrafe();
+				TryAttackTree();
+				MoveTo(GetPreviousWaypoint());
+			}
+			SetAction("Move forward");
+		}
+
+		void Retreat(string param) {
+			MoveTo(GetPreviousWaypoint());
+			if ( lastDistance < MinMoveDistance ) {
+				SetStrafe();
+				TryAttackTree();
+				MoveTo(GetNextWaypoint());
+			}
+			SetAction("Retreat by " + param);
+		}
+
+		void AttackWizard() {
+			AttackLowestHpUnit(nearWizards, "Wizard");
+		}
+
+		void AttackMinion(bool nearest) {
+			if ( nearest ) {
+				AttackNearestUnit(nearMinions, "Minion");
+			} else {
+				AttackLowestHpUnit(nearMinions, "Minion");
+			}
+		}
+
+		void AttackBuilding() {
+			AttackNearestUnit(nearBuildings, "Building");
+		}
+
+		void AttackNeutral() {
+			AttackNearestUnit(nearNeutrals, "Neutral");
+		}
+
+		void TryAttackTree() {
+			var tree = GetNearestUnit(nearTrees);
+			if ( tree != null ) {
+				if ( self.GetDistanceTo(tree) < (self.Radius + tree.Radius) * 1.25D ) {
+					AttackNearestUnit(nearTrees, "Tree");
+				}
+			}
+		}
+
+		// Attack
+		void AttackLowestHpUnit<T>(List<T> units, string param) where T:LivingUnit {
+			var reachableUnits = new List<T>();
+			foreach ( var unit in units ) {
+				var distance = self.GetDistanceTo(unit);
+				if ( distance < self.CastRange ) {
+					double angle = self.GetAngleTo(unit);
+					if ( Math.Abs(angle) < game.StaffSector / 2.0D ) {
+						reachableUnits.Add(unit);
+					}
+				}
+			}
+			if( reachableUnits.Count > 0 ) {
+				var target = GetLowestHPUnit(reachableUnits);
+				SetStrafe();
+				AttackUnit(target);
+				SetAction("Attack lowest hp " + param);
+			} else {
+				AttackNearestUnit(units, param);
+			}
+		}
+
+		void AttackUnit(LivingUnit target) {
+			double angle = self.GetAngleTo(target);
+			move.Turn = angle;
+			move.Action = ActionType.MagicMissile;
+			move.CastAngle = angle;
+			move.MinCastDistance = self.GetDistanceTo(target) - target.Radius + game.MagicMissileRadius;
+		}
+
+		void AttackNearestUnit<T>(List<T> units, string param) where T:LivingUnit {
+			SetStrafe();
+			var target = GetNearestUnit(units);
+			if( target != null ) {
+				var distance = self.GetDistanceTo(target);
+				if( distance < self.CastRange ) {
+					double angle = self.GetAngleTo(target);
+					move.Turn = angle;
+					if ( Math.Abs(angle) < game.StaffSector / 2.0D ) {
+						move.Action = ActionType.MagicMissile;
+						move.CastAngle = angle;
+						move.MinCastDistance = distance - target.Radius + game.MagicMissileRadius;
+						SetAction("Attack " + param);
+						return;
+					}
+				}
+				MoveTo(new Vector2(target.X, target.Y));
+				SetAction("Move to attack " + param);
+			}
+		}
+
+		// Units
+		Faction EnemyFaction {
+			get {
+				if( self.Faction == Faction.Academy ) {
+					return Faction.Renegades;
+				} else {
+					return Faction.Academy;
+				}
+			}
+		}
+
+		Faction FriendFaction {
+			get {
+				if ( self.Faction == Faction.Renegades ) {
+					return Faction.Renegades;
+				} else {
+					return Faction.Academy;
+				}
+			}
+		}
+
+		Faction NeutralFaction {
+			get {
+				return Faction.Neutral;
+			}
+		}
+
+		Faction OtherFaction {
+			get {
+				return Faction.Other;
+			}
+		}
+
+		void SelectNearUnits<T>(T[] from, Faction faction, List<T> to) where T:LivingUnit {
+			to.Clear();
+			foreach( var unit in from ) {
+				if ( unit.Faction == faction ) {
+					if ( self.GetDistanceTo(unit) <= detectDistance ) {
+						to.Add(unit);
+					}
+				}
+			}
+		}
+
+		T GetNearestUnit<T>(List<T> units) where T:LivingUnit {
+			T nearestTarget = null;
+			double nearestTargetDistance = double.MaxValue;
+
+			foreach ( var target in units ) {
+				double distance = self.GetDistanceTo(target);
+
+				if ( distance < nearestTargetDistance ) {
+					nearestTarget = target;
+					nearestTargetDistance = distance;
+				}
+			}
+
+			return nearestTarget;
+		}
+
+		T GetLowestHPUnit<T>(List<T> units) where T:LivingUnit {
+			T lowestHpTarget = null;
+			int lowestHp = int.MaxValue;
+
+			foreach ( var target in units ) {
+				int hp = target.Life;
+
+				if ( hp < lowestHp ) {
+					lowestHpTarget = target;
+					lowestHp = hp;
+				}
+			}
+
+			return lowestHpTarget;
+		}
+
 		// Init
 
 		void TryInit(Wizard self, Game game) {
@@ -176,6 +445,7 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk {
 				waypointsByLane = new Dictionary<LaneType, Vector2[]>();
 
 				waypointsByLane.Add(LaneType.Middle, new Vector2[]{
+					new Vector2(50.0D, mapSize - 50.0D),
 					new Vector2(100.0D, mapSize - 100.0D),
 					GetRandomBool()
 							? new Vector2(600.0D, mapSize - 200.0D)
@@ -185,6 +455,7 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk {
 				});
 
 				waypointsByLane.Add(LaneType.Top, new Vector2[]{
+					new Vector2(50.0D, mapSize - 50.0D),
 					new Vector2(100.0D, mapSize - 100.0D),
 					new Vector2(100.0D, mapSize - 400.0D),
 					new Vector2(200.0D, mapSize - 800.0D),
@@ -199,6 +470,7 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk {
 				});
 
 				waypointsByLane.Add(LaneType.Bottom, new Vector2[]{
+					new Vector2(50.0D, mapSize - 50.0D),
 					new Vector2(100.0D, mapSize - 100.0D),
 					new Vector2(400.0D, mapSize - 100.0D),
 					new Vector2(800.0D, mapSize - 200.0D),
@@ -210,29 +482,7 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk {
 					new Vector2(mapSize - 200.0D, mapSize * 0.5D),
 					new Vector2(mapSize - 200.0D, mapSize * 0.25D),
 					new Vector2(mapSize - 200.0D, 200.0D)
-			});
-
-				switch ( (int)self.Id ) {
-					case 1:
-					case 2:
-					case 6:
-					case 7:
-						curLane = LaneType.Top;
-						break;
-					case 3:
-					case 8:
-						curLane = LaneType.Middle;
-						break;
-					case 4:
-					case 5:
-					case 9:
-					case 10:
-						curLane = LaneType.Bottom;
-						break;
-				}
-
-				curWaypoints = waypointsByLane[curLane];
-
+				});
 			}
 		}
 
